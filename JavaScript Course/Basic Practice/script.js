740972517daebd224121a08505e3884cfd6682bb
M. K. Hamim <mkhamimbd@gmail.com>
2025-11-03 10:34:53 +0600

    practiced object even more, in basic practice file

---
+++ JavaScript Course/Basic Practice/script.js
@@ -0,0 +1,11 @@
+const a = document.getElementById('a');
+
+const user = {
+  name: '',
+  password: 'smith1234'
+};
+
+user.name = 'Hamim';
+
+console.log(user.name);
+console.log(typeof user.name);
