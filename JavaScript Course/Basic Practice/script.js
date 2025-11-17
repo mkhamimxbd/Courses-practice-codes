d20b55028c6dad39e473d00b1e4a463c101c1371
M. K. Hamim <mkhamimbd@gmail.com>
2025-11-17 15:54:53 +0600

    practiced with basic DOM in basic practice file

---
+++ JavaScript Course/Basic Practice/script.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = `
+  <h1>Hello World</h1>
+  <p style='font-family: arial;'>This is a webpage, but the HTML is mainly written in <strong>javascript</strong> file. isnt that cool?</p>
+`
