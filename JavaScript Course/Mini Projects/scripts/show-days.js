f74b33efa10f6d76149dc59b5458c473db9eeeb1
M. K. Hamim <mkhamimbd@gmail.com>
2025-12-18 19:27:25 +0600

    combined js code to html file for show-days.html

--- JavaScript Course/Mini Projects/scripts/show-days.js
+++ JavaScript Course/Mini Projects/scripts/show-days.js
@@ -1,15 +1,3 @@
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-
-
-function showDate(days) {
-  const daysLater = dayjs().add(days, 'day').format('MMMM DD');
-  return daysLater;
-}
-
-
-
-document.querySelector('.js-date')
-  .innerHTML = showDate(2);
 
 
 
